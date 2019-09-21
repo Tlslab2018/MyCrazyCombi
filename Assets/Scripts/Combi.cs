5a917c36582c81d461461f22527d07caa4d7eb40
Tlslab2018 <tlslab2018@gmail.com>
2019-09-20 20:09:36 -0500

    Combi move and jump

---
+++ Assets/Scripts/Combi.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combi : MonoBehaviour
+{
+
+    private bool canJump = true;
+    private int currentPosition = 1;
+
+    public int[] xPosition;
+
+    public Rigidbody rb;
+    public float jumpForce = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = new Vector3(0, 1, -20);
+        pos.x = xPosition[currentPosition];
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentPosition--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentPosition++;
+        }
+
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            canJump = false;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+
+
+        if (currentPosition < 0)
+            currentPosition = 0;
+
+        //if (currentPosition > 2)
+        //    currentPosition = 2;
+        if (currentPosition >= xPosition.Length)
+            currentPosition = xPosition.Length-1;
+        
+
+        Vector3 pos = transform.position;
+        pos.x = xPosition[currentPosition];
+        transform.position = pos;
+    }
+
+    void FixedUpdate() {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        canJump = true;
+    }
+
+
+}
